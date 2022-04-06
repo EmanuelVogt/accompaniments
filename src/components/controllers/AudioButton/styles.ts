e8365260f725ff9de85d28246608346b5cfb6287
EmanuelVogt <contatoemanuel3@gmail.com>
2022-04-06 15:00:25 -0300

    chage conflict name component

--- src/components/controllers/AudioButton/styles.ts
+++ src/components/controllers/AudioButton/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.View`
   margin-top: ${RFValue(8)}px;
   margin-bottom: ${RFValue(20)}px;
 `
-export const Audio = styled.TouchableOpacity`
+export const AudioSubContainer = styled.TouchableOpacity`
   width: 100%;
   flex-direction: row;
   justify-content: space-between;
