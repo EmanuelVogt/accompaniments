57c98d9407fd331992c917063cdd1a6885a9f830
EmanuelVogt <contatoemanuel3@gmail.com>
2022-04-06 16:01:01 -0300

    create AudioPlayer component

---
+++ src/components/controllers/AudtioPlayer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container, PlayerButton } from './styles'
+
+export function AudioPlayer() {
+  return (
+    <Container>
+      <PlayerButton name="play-circle" />
+    </Container>
+  )
+}
---
+++ src/components/controllers/AudtioPlayer/styles.ts
@@ -0,0 +1,8 @@
+import { FontAwesome5 } from '@expo/vector-icons'
+import styled from 'styled-components/native'
+export const Container = styled.View`
+  width: 100%;
+`
+export const PlayerButton = styled(FontAwesome5)`
+  font-size: 24px;
+`
